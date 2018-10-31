6633bf99bfc8c4f19fb634554369bbe18976877a
Corey Hunt <corey.sid.hunt@gmail.com>
2018-10-31 09:39:13 -0600

    Added setUp function and made other corrections

--- test_service.py
+++ test_service.py
@@ -3,6 +3,9 @@ import mock
 from service import Service
 
 class TestService(unittest.TestCase):
+    def setUp(self):
+        self.service = Service()
+
     def test_bad_random(self):
         pass
 
@@ -10,20 +13,22 @@ class TestService(unittest.TestCase):
         pass
 
     def test_abs_plus(self):
-        service = Service()
-
         # Negative
-        self.assertEqual(service.abs_plus(-1), 2)
+        self.assertEqual(self.service.abs_plus(-1), 2)
 
         # 0
-        self.assertEqual(service.abs_plus(0), 1)
+        self.assertEqual(self.service.abs_plus(0), 1)
 
         # Positive
-        self.assertEqual(service.abs_plus(1), 2)
+        self.assertEqual(self.service.abs_plus(1), 2)
 
         # Non-integer
         with self.assertRaises(TypeError):
-            service.abs_plus('a')
+            self.service.abs_plus('a')
+
+        # No argument
+        with self.assertRaises(TypeError):
+            self.service.abs_plus()
         
 
     def test_complicated_function(self):
